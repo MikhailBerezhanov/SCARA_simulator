761f689646a1280b8f8c9580fdab5151a23afbc7
Mikhail Berezhanov <berezhanov.m@gmail.com>
2024-01-28 13:03:59 +0400

    refactoring , add_gripper started

--- main.py
+++ main.py
@@ -22,10 +22,9 @@ origin_point_x = origin_point[0]
 origin_point_y = origin_point[1]
 
 
-# SCARA scheme
-# BaseLink - Joint1 - Link1 - Joint2 - Link2
-
-my_scara = scara.Scara(origin_point)
+# SCARA nodel:
+# Base - Joint1 - Link1 - Joint2 - Link2 - Gripper
+my_scara = scara.ScaraModel(origin_point)
 
 my_scara.add_base()
 my_scara.add_joint()
@@ -34,6 +33,8 @@ my_scara.add_joint()
 my_scara.add_link(length = 80, start_angle = 120)
 my_scara.add_joint()
 my_scara.add_link(length = 40, start_angle = 200)
+
+my_scara.add_gripper()
 
 # my_scara.add_joint()
 # my_scara.add_link(length = 120, start_angle = 30)
@@ -44,119 +45,29 @@ my_scara.add_link(length = 40, start_angle = 200)
 for link in my_scara.links:
     link.add_angle_arc()
 
-# my_scara.links[0].add_angle_arc()
-# my_scara.links[1].add_angle_arc()
 
-# my_scara.add_angle_arc(link_index = 0)
-# my_scara.add_angle_arc(link_index = 1)
-# my_scara.add_angle_arc(link_index = 2)
-
-# scara_batch = pyglet.graphics.Batch()
-
-# base_side_len = 20
-# base_point_x = origin_point_x - base_side_len // 2
-# base_point_y = origin_point_y - base_side_len // 2
-
-# base_link = pyglet.shapes.Box(base_point_x, base_point_y, 
-#                               base_side_len, base_side_len, 
-#                               thickness=1, color=(123, 255, 120), batch=scara_batch)
-
-# joint_radius = 4
-
-# joint1 = pyglet.shapes.Circle(origin_point_x, origin_point_y,
-#                               joint_radius, 
-#                               segments=None, 
-#                               color=(255, 255, 255, 255), 
-#                               batch=scara_batch)
-
-
-# link_width = 2
-
-# link1_start_x = joint1.x
-# link1_start_y = joint1.y
-# link1_len = 80
 link1_angle = 0
 link2_angle = 0
-# link1 = pyglet.shapes.Line(link1_start_x, link1_start_y,
-#                            link1_start_x + link1_len * math.cos(math.radians(link1_angle)),
-#                            link1_start_y + link1_len * math.sin(math.radians(link1_angle)),
-#                            width=link_width, color=(120, 110, 50), batch=scara_batch)
-
-# angle_arc_radius = 28
-# angle_value_format = "{:.1f}"
-
-# link1_arc = pyglet.shapes.Arc(link1_start_x, link1_start_y, 
-#                               angle_arc_radius, 
-#                               segments=None, 
-#                               angle=math.radians(link1.rotation), 
-#                               start_angle=0, 
-#                               closed=False, 
-#                               color=(120, 120, 120, 200), 
-#                               batch=scara_batch)
-
-# link1_arc_label = pyglet.text.Label(text=angle_value_format.format(link1_arc.angle),
-#                                  font_size=10,
-#                                  color=(120, 120, 120, 200),
-#                                  x=link1_start_x + angle_arc_radius + 18,
-#                                  y=link1_start_y + angle_arc_radius - 8,
-#                                  anchor_x='center',
-#                                  anchor_y='center',
-#                                  batch=scara_batch)
-
-
-# joint2 = pyglet.shapes.Circle(link1.x2, link1.y2,
-#                               joint_radius, 
-#                               segments=None, 
-#                               color=(255, 255, 255, 255), 
-#                               batch=scara_batch)
-
-# link2 = pyglet.shapes.Line(joint2.x, joint2.y,
-#                            343, 123,
-#                            width=link_width, color=(20, 200, 100), batch=scara_batch)
-
-# Clockwise rotation of the shape in degrees
-# It will be rotated about its (anchor_x, anchor_y) position, 
-# which defaults to the first vertex point of the shape.
-# Can be used to rotate line
-#  line.rotation = -90 
 
 def update_link1(dt):
     global link1_angle
     global link2_angle
 
-    # Update the end point of the line based on the new angle
     link1_angle += 30 * dt
     link2_angle += 90 * dt
 
     my_scara.joints[0].rotate(link1_angle)
     my_scara.joints[1].rotate(link2_angle)
 
+    my_scara.joints[2].rotate(-link2_angle)
 
-    # rad = math.radians(link1_angle)
-
-    # # link1.rotation = -link1_angle
-
-    # link1.x2 = link1_start_x + link1_len * math.cos(rad)
-    # link1.y2 = link1_start_y + link1_len * math.sin(rad)
-
-    # arc1_angle_value = link1_angle % 360
-    # link1_arc.angle = math.radians(arc1_angle_value)
-    # link1_arc_label.text = angle_value_format.format(arc1_angle_value)
-
-    # joint2.x = link1.x2
-    # joint2.y = link1.y2
-
-    # link2.x = joint2.x
-    # link2.y = joint2.y
 
 @window.event
 def on_draw():
     window.clear()
     axes.draw()
-    # scara_batch.draw()
     my_scara.draw()
     
-
 
 pyglet.clock.schedule_interval(update_link1, 1 / 60.0)  # Update at 60 FPS
 pyglet.app.run()
--- scara.py
+++ scara.py
@@ -1,20 +1,18 @@
 import pyglet
 import math
 
-
-
-# Base:Joint1 - Link1 - Joint2 - Link2
 # Joint -> Link
 # Link (->) Joint 
 
 class Joint:
+    JOINT_RADIUS = 4
 
     def __init__(self, start_point_x, start_point_y, batch, link = None):
         self.link = link
 
         self.shape = pyglet.shapes.Circle(
             start_point_x, start_point_y,
-            4, 
+            self.JOINT_RADIUS, 
             segments=None, 
             color=(255, 255, 255, 255), 
             batch=batch)
@@ -27,19 +25,18 @@ class Joint:
             self.link.rotate(angle)
 
     def move(self, x, y):
-
         self.shape.x = x
         self.shape.y = y
 
         if self.link:
             self.link.move(x, y)
 
-
-
 class Link:
     LINK_WIDTH = 2
     ANGLE_ARC_RADIUS = 28
     ANGLE_VALUE_FMT = "{:.0f}"
+    ANGLE_VALUE_OFFSET_X = 10
+    ANGLE_VALUE_OFFSET_Y = -8
 
     def __init__(self, start_joint, end_joint, batch, length, start_angle):
         self.angle_arc = None
@@ -52,21 +49,23 @@ class Link:
         self.start_joint = start_joint
         self.end_joint = end_joint
 
-        self.link = pyglet.shapes.Line(
+        self.line = pyglet.shapes.Line(
             start_joint.shape.x, start_joint.shape.y,
             start_joint.shape.x + length * math.cos(math.radians(start_angle)),
             start_joint.shape.y + length * math.sin(math.radians(start_angle)),
             width=self.LINK_WIDTH, color=(120, 110, 50), batch=batch)
 
+        self.start_joint.connect_link(self)
+
     def add_angle_arc(self):
-        start_x = self.link.x
-        start_y = self.link.y
+        start_x = self.line.x
+        start_y = self.line.y
 
         self.angle_arc = pyglet.shapes.Arc(
             start_x, start_y, 
             self.ANGLE_ARC_RADIUS, 
             segments=None, 
-            angle=math.radians(self.link.rotation),
+            angle=math.radians(self.line.rotation),
             start_angle=0, 
             closed=False, 
             color=(120, 120, 120, 200), 
@@ -76,23 +75,19 @@ class Link:
             text=self.ANGLE_VALUE_FMT.format(math.degrees(self.angle_arc.angle)),
             font_size=10,
             color=(120, 120, 120, 200),
-            x=start_x + self.ANGLE_ARC_RADIUS + 18,
-            y=start_y + self.ANGLE_ARC_RADIUS - 8,
+            x=start_x + self.ANGLE_ARC_RADIUS + self.ANGLE_VALUE_OFFSET_X,
+            y=start_y + self.ANGLE_ARC_RADIUS + self.ANGLE_VALUE_OFFSET_Y,
             anchor_x='center',
             anchor_y='center',
             batch=self.batch)
 
     def rotate(self, angle):
-        # pass
-        # TODO:
-        # 1. update end joint position if any
-        # 2. update angle arc
-
         rad = math.radians(angle)
         self.angle = angle
 
-        self.link.x2 = self.start_joint.shape.x + self.len * math.cos(rad)
-        self.link.y2 = self.start_joint.shape.y + self.len * math.sin(rad)
+        # Update the end point of the line based on the new angle
+        self.line.x2 = self.start_joint.shape.x + self.len * math.cos(rad)
+        self.line.y2 = self.start_joint.shape.y + self.len * math.sin(rad)
 
         if self.angle_arc and self.angle_arc_label:
             arc_angle_value = self.angle % 360
@@ -100,44 +95,29 @@ class Link:
             self.angle_arc_label.text = self.ANGLE_VALUE_FMT.format(arc_angle_value)
 
         if self.end_joint:
-            self.end_joint.move(self.link.x2, self.link.y2)
-
-            # self.end_joint.x = self.link.x2
-            # self.end_joint.y = self.link.y2
+            self.end_joint.move(self.line.x2, self.line.y2)
 
     def move(self, x, y):
-        self.link.x = x
-        self.link.y = y
+        self.line.x = x
+        self.line.y = y
 
-        # update x2, y2 point
+        # Update the end point
         rad = math.radians(self.angle)
-        self.link.x2 = self.start_joint.shape.x + self.len * math.cos(rad)
-        self.link.y2 = self.start_joint.shape.y + self.len * math.sin(rad)
+        self.line.x2 = self.start_joint.shape.x + self.len * math.cos(rad)
+        self.line.y2 = self.start_joint.shape.y + self.len * math.sin(rad)
 
         if self.angle_arc and self.angle_arc_label:
             self.angle_arc.x = x
             self.angle_arc.y = y
-            self.angle_arc_label.x = x
-            self.angle_arc_label.y = y
+            self.angle_arc_label.x = x + self.ANGLE_ARC_RADIUS + self.ANGLE_VALUE_OFFSET_X
+            self.angle_arc_label.y = y + self.ANGLE_ARC_RADIUS + self.ANGLE_VALUE_OFFSET_Y
 
         if self.end_joint:
-            self.end_joint.move(self.link.x2, self.link.y2)
+            self.end_joint.move(self.line.x2, self.line.y2)
 
-# Base - Joint1 - Link1 - Joint2 - Link2
-class Scara:
+class ScaraModel:
     BASE_LEN = 20
-    JOINT_RADIUS = 4
-
-    # TODO;
-    # class Joint:
-
-    #     def __init__(self, start_x, start_y, batch):
-    #         self.joint = pyglet.shapes.Circle(
-    #             start_point_x, start_point_y,
-    #             self.JOINT_RADIUS, 
-    #             segments=None, 
-    #             color=(255, 255, 255, 255), 
-    #             batch=batch)
+    GRIPPER_SIDE = 10
 
     def __init__(self, origin_point=(0, 0)):
         self.batch = pyglet.graphics.Batch()
@@ -148,36 +128,29 @@ class Scara:
 
         self.joints = []
         self.links = []
-
-        self.arc = None 
-        self.arc_label = None 
+        self.gripper = None
 
     def draw(self):
         self.batch.draw()
 
     def add_base(self):
-        self.base = pyglet.shapes.Box(
+        self.base = pyglet.shapes.Rectangle(
             self.base_x, self.base_y, 
             self.BASE_LEN, self.BASE_LEN, 
-            thickness=1, color=(123, 255, 120), batch=self.batch)
+            color=(123, 255, 120), batch=self.batch)
 
     def add_joint(self):
         if not self.links:
-            start_point_x = self.base_x + self.BASE_LEN // 2
-            start_point_y = self.base_y + self.BASE_LEN // 2
+            start_x = self.base_x + self.BASE_LEN // 2
+            start_y = self.base_y + self.BASE_LEN // 2
         else:
-            start_point_x = self.links[-1].link.x2
-            start_point_y = self.links[-1].link.y2
+            if self.links[-1].end_joint:
+                raise Exception("Add joint failed - no free links available")
 
+            start_x = self.links[-1].line.x2
+            start_y = self.links[-1].line.y2
 
-        joint = Joint(start_point_x, start_point_y, self.batch)
-
-        # joint = pyglet.shapes.Circle(
-        #     start_point_x, start_point_y,
-        #     self.JOINT_RADIUS, 
-        #     segments=None, 
-        #     color=(255, 255, 255, 255), 
-        #     batch=self.batch)
+        joint = Joint(start_x, start_y, self.batch)
 
         if self.links:
             self.links[-1].end_joint = joint
@@ -188,18 +161,19 @@ class Scara:
         if not self.joints:
             raise Exception("Add link failed - no joints available")
 
-        # start_x = self.joints[-1].x
-        # start_y = self.joints[-1].y
-
         link = Link(self.joints[-1], None, self.batch, length, start_angle)
-
-        self.joints[-1].connect_link(link)
 
         self.links.append(link)
 
+    def add_gripper(self):
+        if not self.links or self.links[-1].end_joint:
+            raise Exception("Add gripper failed - no free links available")
 
-    def update(self): 
-        pass
-            # arc1_angle_value = link1_angle % 360
-            # link1_arc.angle = math.radians(arc1_angle_value)
-            # link1_arc_label.text = angle_value_format.format(arc1_angle_value)
+        start_x = self.links[-1].line.x2
+        start_y = self.links[-1].line.y2
+
+        self.gripper = pyglet.shapes.Box(
+            start_x - self.GRIPPER_SIDE // 2 , start_y - self.GRIPPER_SIDE // 2, 
+            self.GRIPPER_SIDE, self.GRIPPER_SIDE, 
+            thickness=1, color=(255, 255, 255, 255), batch=self.batch)
+
